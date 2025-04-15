7e105c00afe5bb6c31bf3777fac164e01d6e7f49
jngmnj <exorbitance@naver.com>
2025-04-15 19:30:17 +0900

    ⚰️ Chore: 불필요한 페이지 파일 삭제, 에러페이지추가, 라우팅 수정(#5)
    - 불필요한 페이지 삭제
    - App.tsx 라우팅 수정(삭제 및 에러페이지 추가)
    - 에러페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DefaultLayout from './layout/DefaultLayout';
-import AboutPage from './pages/AboutPage';
-import BlogPage from './pages/BlogPage';
-import CartPage from './pages/CartPage';
-import ContactPage from './pages/ContactPage';
-import FavoritesPage from './pages/FavoritesPage';
+import ErrorPage from './pages/ErrorPage';
 import MainPage from './pages/MainPage';
 import ProductPage from './pages/ProductPage';
-import SearchPage from './pages/SearchPage';
-import UserPage from './pages/UserPage';
 
 function App() {
   return (
@@ -18,13 +12,7 @@ function App() {
           <Route path="/" element={<DefaultLayout />}>
             <Route index element={<MainPage />} />
             <Route path="products" element={<ProductPage />} />
-            <Route path="about" element={<AboutPage />} />
-            <Route path="blog" element={<BlogPage />} />
-            <Route path="cart" element={<CartPage />} />
-            <Route path="contact" element={<ContactPage />} />
-            <Route path="favorites" element={<FavoritesPage />} />
-            <Route path="user" element={<UserPage />} />
-            <Route path="search" element={<SearchPage />} />
+            <Route path="*" element={<ErrorPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
