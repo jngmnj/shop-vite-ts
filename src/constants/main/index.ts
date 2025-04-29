e310e4e546262dda0aed5c7a82dc776249d1d30f
jngmnj <exorbitance@naver.com>
2025-04-29 20:29:54 +0900

    ✨ Feat: MainBanner UI 작업
    - 메인페이지 컨텐츠들은 constants로 관리
      - src/constants/main/index.ts
    - MainBanner.tsx: 메인 히어로배너 작업
      - 어두운배너인지 밝은배너인지 따라 텍스트, 버튼 컬러 지정
      - 타이틀 강조 따로 지정
    - 링크추가 안됨

---
+++ src/constants/main/index.ts
@@ -0,0 +1,13 @@
+// MainBanner
+
+export const MAIN_BANNER_CONTENT = {
+  title: 'IPhone 14 Pro',
+  highlight: 'Pro',
+  subtitle: 'Pro.Beyond.',
+  description: 'Created to change everything for the better. For everyone',
+  imageSrc: '/assets/images/main/mainBanner/Iphone_Image.png',
+  backgroundStyle: {
+    background: `#211C24`,
+  },
+  isBannerDark: true,
+};
