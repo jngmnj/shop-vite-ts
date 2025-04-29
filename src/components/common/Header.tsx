30f9e63701654fc20d5797d0c89e5624cd342bda
jngmnj <exorbitance@naver.com>
2025-04-29 20:27:31 +0900

    💄 Style: Header 컴포넌트 모바일퍼스트 수정
    - Header.tsx: 모바일퍼스트 반응형 수정
      - Navbar.tsx
      - SearchBar.tsx
    - 추후 사이드메뉴 개발 필요

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,33 +1,44 @@
 import cartIcon from '@/assets/icons/32px/Cart.svg';
 import favorIcon from '@/assets/icons/32px/Favorites.svg';
 import userIcon from '@/assets/icons/32px/User.svg';
+import sideMenuIcon from '@/assets/icons/40px/Burger.svg';
 import logo from '@/assets/images/common/Logo.png';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import NavBar from './NavBar';
 import SearchBar from './SearchBar';
 
-const Header = () => (
-  <header className="border-b border-b-[#B5B5B5] p-4">
-    <div className="inner flex items-center justify-between gap-14">
-      <Link to="/">
-        <img src={logo} />
-      </Link>
-      <SearchBar />
-      <NavBar />
-      <div className="flex gap-6">
-        <Link to="/favorites">
-          <img src={favorIcon} />
+const Header = () => {
+  const [isSideOpen, setIsSideOpen] = useState(false);
+  const toggleSideMenu = () => {
+    setIsSideOpen((prev) => !prev);
+  };
+  return (
+    <header className="h-22 border-b border-b-[#B5B5B5] p-4 md:h-auto">
+      <div className="inner flex h-full items-center justify-between gap-14">
+        <Link to="/">
+          <img src={logo} alt="logo" />
         </Link>
-        <Link to="/cart">
-          <img src={cartIcon} />
-        </Link>
-        <Link to="/user">
-          <img src={userIcon} />
-        </Link>
+        <SearchBar />
+        <NavBar />
+        <div className="hidden gap-6 md:flex">
+          <Link to="/favorites">
+            <img src={favorIcon} alt="favorites" />
+          </Link>
+          <Link to="/cart">
+            <img src={cartIcon} alt="cart" />
+          </Link>
+          <Link to="/user">
+            <img src={userIcon} alt="user" />
+          </Link>
+        </div>
+        <button className="flex md:hidden" onClick={toggleSideMenu}>
+          <img src={sideMenuIcon} alt="user" />
+        </button>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
