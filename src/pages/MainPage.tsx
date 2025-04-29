32e6e7797295b2f531bbc1492ad9191e7781dbe7
jngmnj <exorbitance@naver.com>
2025-04-29 20:22:18 +0900

    ✨ Feat: 메인페이지 컴포넌트 파일 생성, 추가(#4)
    - MainPage.tsx 메인페이지에 메인페이지 컴포넌트 추가
    - 메인페이지 컴포넌트 파일 생성
      - ProdudctGrid
      - CategoryTab
      - ProductList
      - ProductItem
      - BottomBanner
      - PromotionBanner

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,10 @@
 import MetaTag from '@/components/common/MetaTag';
+import BottomBanner from '@/components/main/BottomBanner';
+import CategoryTab from '@/components/main/CategoryTab';
+import MainBanner from '@/components/main/MainBanner';
+import ProductGrid from '@/components/main/ProductGrid';
+import PromotionBanner from '@/components/main/PromotionBanner';
+import ProductList from '@/components/products/ProductList';
 
 const MainPage = () => {
   return (
@@ -8,7 +14,17 @@ const MainPage = () => {
         url="https://cyber.com"
         keywords="cyber, shop, online, shopping, Apple, iphone, macbook, ipods, ipods max, galaxy"
       />
-      <div>Main</div>
+      <div>
+        <MainBanner />
+        <ProductGrid />
+        <CategoryTab />
+        {/* main product */}
+        <ProductList />
+        <BottomBanner />
+        {/* discount product */}
+        <ProductList />
+        <PromotionBanner />
+      </div>
     </>
   );
 };
