6079f536e00a41502ecefe90df0b965702c70561
jngmnj <exorbitance@naver.com>
2025-05-27 20:05:47 +0900

    ✨ Feat: Button 컴포넌트 생성(#27)
    - 공통 버튼 컴포넌트(Button) 생성
    - variant: filled, outlined, white-outlined 스타일 지원
    - size: lg, xl 버튼 크기 적용
    - className은 유틸 함수 cn으로 병합 처리
    - href가 있을 경우 React Router의 <Link>로 렌더링, 없을 경우 <button> 사용

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,56 @@
+import '@/styles/button.css';
+import { cn } from '@/utils/style';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  type?: 'button' | 'submit' | 'reset';
+  href?: string;
+  children: React.ReactNode;
+  className?: string;
+  variant?: 'filled' | 'outlined' | 'white-outlined';
+  size?: 'lg' | 'xl';
+}
+const baseClass = 'btn';
+const sizeMap = {
+  lg: 'btn-lg',
+  xl: 'btn-xl',
+};
+const variantMap = {
+  filled: 'btn-filled',
+  outlined: 'btn-outlined',
+  'white-outlined': 'btn-white-outlined',
+};
+
+const Button = ({
+  children,
+  href,
+  type,
+  className,
+  variant = 'filled',
+  size = 'lg',
+  ...rest
+}: ButtonProps) => {
+  const finalClass = cn(
+    baseClass,
+    sizeMap[size],
+    variantMap[variant],
+    className,
+  );
+
+  if (href) {
+    return (
+      <Link to={href} className={finalClass}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button type={type} className={finalClass} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
