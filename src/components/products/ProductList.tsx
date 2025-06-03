72606743540e638b59b571dcc114dc35d26aece5
jngmnj <exorbitance@naver.com>
2025-06-03 15:43:11 +0900

    ✨ Feat: ProductList, ProductItem 컴포넌트 개발
    - ProductList.tsx
    - ProductItem.tsx
      - 좋아요Lottie 토글버튼 구현(성능 이슈, 리팩토링필요)
    - package.json
      - lottie-react 추가
    - 에셋추가
      - like.json, liked.json, liked.svg
    - MainPage에 카테고리 메뉴와 함께 UI 적용

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,5 +1,22 @@
-const ProductList = () => {
-  return <div>ProductList</div>;
+import { handleImageError } from '@/utils/handleImageError';
+import ProductItem from './ProductItem';
+
+interface ProductListProps {
+  products: {
+    id: number;
+    imgSrc: string;
+    name: string;
+    price: number;
+  }[];
+}
+const ProductList = ({ products }: ProductListProps) => {
+  return (
+    <ul className="grid grid-cols-2 gap-4 lg:grid-cols-4">
+      {products.map((item) => (
+        <ProductItem key={item.id} item={item} onError={handleImageError} />
+      ))}
+    </ul>
+  );
 };
 
 export default ProductList;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,53 @@
-import MetaTag from '@/components/common/MetaTag';
-import BottomBanner from '@/components/main/BottomBanner';
-import CategoryTab from '@/components/main/CategoryTab';
-import MainBanner from '@/components/main/MainBanner';
-import ProductGrid from '@/components/main/ProductGrid';
-import PromotionBanner from '@/components/main/PromotionBanner';
-import ProductList from '@/components/products/ProductList';
+import MetaTag from '@components/common/MetaTag';
+import BottomBanner from '@components/main/BottomBanner';
+import CategoryTab from '@components/main/CategoryTab';
+import MainBanner from '@components/main/MainBanner';
+import ProductGrid from '@components/main/ProductGrid';
+import PromotionBanner from '@components/main/PromotionBanner';
+import ProductList from '@components/products/ProductList';
+import { useState } from 'react';
+
+const dummyCategories = [
+  { id: 1, name: 'All', href: '/' },
+  { id: 2, name: 'Clothes', href: '/' },
+  { id: 3, name: 'Electronics', href: '/' },
+  { id: 4, name: 'Furniture ', href: '/' },
+  { id: 5, name: 'Shoes', href: '/' },
+];
+
+const dummyProducts = [
+  {
+    id: 1,
+    imgSrc: 'assets/images/common/img_og.png',
+    name: 'Apple iPhone 14 Pro Max 128GB Deep Purple ',
+    price: 900,
+  },
+  {
+    id: 2,
+    imgSrc: 'assets/images/common/img_og.png',
+    name: 'Apple iPhone 14 Pro Max 128GB Deep Purple ',
+    price: 90000,
+  },
+  {
+    id: 3,
+    imgSrc: 'assets/images/common/img_og.png',
+    name: 'Apple iPhone 14 Pro Max 128GB Deep Purple ',
+    price: 900,
+  },
+  {
+    id: 4,
+    imgSrc: 'assets/images/common/img_og123.png',
+    name: 'Apple iPhone 14 Pro Max 128GB Deep Purple ',
+    price: 900,
+  },
+];
 
 const MainPage = () => {
+  const [activeCategory, setActiveCategory] = useState('All');
+  const handleCategoryClick = (category: string) => {
+    setActiveCategory(category);
+  };
+
   return (
     <>
       <MetaTag
@@ -19,10 +60,26 @@ const MainPage = () => {
         <ProductGrid />
         <CategoryTab />
         {/* main product */}
-        <ProductList />
+        <div className="inner px-4 py-14 lg:px-0">
+          <ul className="mb-8 flex touch-auto gap-6 overflow-x-auto whitespace-nowrap">
+            {dummyCategories.map((category) => (
+              <li key={category.id} className="">
+                <button
+                  type="button"
+                  className="block cursor-pointer p-2 text-lg font-bold text-[#8B8B8B] hover:text-gray-600"
+                  style={
+                    activeCategory === category.name ? { color: '#000' } : {}
+                  }
+                  onClick={() => handleCategoryClick(category.name)}
+                >
+                  {category.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <ProductList products={dummyProducts} />
+        </div>
         <BottomBanner />
-        {/* discount product */}
-        <ProductList />
         <PromotionBanner />
       </div>
     </>
