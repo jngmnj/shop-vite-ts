0d13a666e6e71e97f8d9caab8e2e655bec29eb52
jngmnj <exorbitance@naver.com>
2025-06-09 23:07:39 +0900

    ✨ Feat: Breadcrumb 컴포넌트 개발
    {label, href}의 배열을 받음
    마지막 아이템에는 현재 페이지므로 링크 필요없음

---
+++ src/components/products/BreadCrumb.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import arrowIcon from '@assets/icons/24px/Arrow.svg';
+
+type Crumb = {
+  label: string;
+  href?: string;
+};
+
+type BreadcrumbProps = {
+  items: Crumb[];
+};
+
+const BreadCrumb: React.FC<BreadcrumbProps> = ({ items }) => {
+  return (
+    <div className="inner py-10">
+      <nav className="flex items-center gap-4 text-gray-500">
+        {items.map((item, idx) => (
+          <React.Fragment key={idx}>
+            {item.href ? (
+              <a href={item.href} className="text-gray-400 hover:underline">
+                {item.label}
+              </a>
+            ) : (
+              <span className="font-semibold text-black">{item.label}</span>
+            )}
+            {idx < items.length - 1 && (
+              <span className="text-gray-300">
+                <img
+                  src={arrowIcon}
+                  alt="Arrow"
+                  className="inline-block h-4 w-4"
+                />
+              </span>
+            )}
+          </React.Fragment>
+        ))}
+      </nav>
+    </div>
+  );
+};
+
+export default BreadCrumb;
