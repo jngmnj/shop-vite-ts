305563c30897a45ca8b75734fac8741d76d3a320
jngmnj <exorbitance@naver.com>
2025-06-17 21:26:05 +0900

    ✨ Feat: 상품 아이템, 상품리스트 컴포넌트수정
    - 좋아요 기능 보류
    - 상품리스트: 그리드 컬럼 props로 받기

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,17 +1,32 @@
 import { handleImageError } from '@/utils/handleImageError';
+
 import ProductItem from './ProductItem';
 
 interface ProductListProps {
   products: {
     id: number;
-    imgSrc: string;
-    name: string;
+    images: string[];
+    title: string;
     price: number;
   }[];
+  cols?: number;
+  error?: Error | null;
 }
-const ProductList = ({ products }: ProductListProps) => {
+const ProductList = ({ products, cols, error }: ProductListProps) => {
+  let colClass = '';
+  if (cols) {
+    colClass =
+      {
+        1: 'lg:grid-cols-1',
+        2: 'lg:grid-cols-2',
+        3: 'lg:grid-cols-3',
+        4: 'lg:grid-cols-4',
+        5: 'lg:grid-cols-5',
+      }[cols] || 'lg:grid-cols-4';
+  }
+
   return (
-    <ul className="grid grid-cols-2 gap-4 lg:grid-cols-4">
+    <ul className={`grid gap-4 ${colClass} grid-cols-2`}>
       {products.map((item) => (
         <ProductItem key={item.id} item={item} onError={handleImageError} />
       ))}
